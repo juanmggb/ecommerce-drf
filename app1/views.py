0caf54953be77c4998752f460141413954d78cef
JuanM-GG <juanmanuelgutierrezgarcia@outlook.com>
2023-07-21 18:07:45 -0600

    scond cimmit

--- app1/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render, HttpResponse
-
-# Create your views here.
-def hello(request):
-
-    # return HttpResponse("hello")
-    return render(request, "app1/base.html", context={
-        "x" : 4, "y": 78,
-    })
